e641c6326c5e4a7728229f0f42a8261f9a87345e
cleme <malcia.mrg@gmail.com>
2024-07-07 02:05:42 +0200

    swagger

---
+++ src/main/java/com/maliciamrg/lrcat/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.maliciamrg.lrcat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
