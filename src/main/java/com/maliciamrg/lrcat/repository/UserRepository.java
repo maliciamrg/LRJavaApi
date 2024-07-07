4c86d08063575402df98bf4e2bf4cf554cfcb3b4
cleme <malcia.mrg@gmail.com>
2024-07-07 21:54:27 +0200

    file

--- src/main/java/com/maliciamrg/lrcat/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maliciamrg.lrcat.repository;
-
-import com.maliciamrg.lrcat.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
