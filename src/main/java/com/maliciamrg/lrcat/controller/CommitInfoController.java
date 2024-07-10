b1ae8bd3fedcb779035723c318c4549cf9337421
cleme <malcia.mrg@gmail.com>
2024-07-10 10:41:52 +0200

    externalize persitence

--- src/main/java/com/maliciamrg/lrcat/controller/CommitInfoController.java
+++ src/main/java/com/maliciamrg/lrcat/controller/CommitInfoController.java
@@ -1,6 +1,7 @@
 package com.maliciamrg.lrcat.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class CommitInfoController {
     @Value("${git.commit.id}")
     private String commitId;
 
-    @RequestMapping("/commitId")
+    @GetMapping("/commitId")
     public Map<String, String> getCommitId() {
         Map<String, String> result = new HashMap<>();
         result.put("Commit message",commitMessage);
